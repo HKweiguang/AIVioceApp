af4941b2c401d4a885e0030d973e81ee13c72619
huangk <huangk@robooot.com>
2021-12-15 17:57:32 +0800

    update gradle to .kts

---
+++ module_weather/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+    buildToolsVersion = AppConfig.buildToolsVersion
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        consumerProguardFile("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    // 依赖操作
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(dependenciesConfig.CORE_KTX)
+    implementation(dependenciesConfig.APPCOMPAT)
+    implementation(dependenciesConfig.MATERIAL)
+}
