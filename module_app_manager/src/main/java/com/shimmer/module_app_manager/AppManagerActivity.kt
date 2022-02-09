eb6b7fefc8a6c99adf049e6b3f08bc275a9653e6
huangk <huangk@robooot.com>
2022-02-09 18:09:02 +0800

    add 自定义分页抽屉View:1

--- module_app_manager/src/main/java/com/shimmer/module_app_manager/AppManagerActivity.kt
+++ module_app_manager/src/main/java/com/shimmer/module_app_manager/AppManagerActivity.kt
@@ -6,6 +6,7 @@ import com.shimmer.lib_base.helper.ARouterHelper
 
 @Route(path = ARouterHelper.PATH_APP_MANAGER)
 class AppManagerActivity : BaseActivity() {
+
     override fun getLayoutId() = R.layout.activity_app_manager
 
     override fun getTitleText() = getString(R.string.app_title_app_manager)
